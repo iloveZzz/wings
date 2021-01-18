b2cec51d6e3dc30bf5b581227c8c773ac5c3c945
daomingzhu <596167028@qq.com>
2021-01-18 19:42:01 +0800

    no message

---
+++ micro-services/wings-authentication/src/main/java/com/works/wings/wingssecurity/WingsSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.works.wings.wingssecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WingsSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WingsSecurityApplication.class, args);
+    }
+
+}
