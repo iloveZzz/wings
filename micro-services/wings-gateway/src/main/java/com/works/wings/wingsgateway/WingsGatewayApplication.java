a4c783cae2ba564bc9fb1d8e154aafb2bb464376
zhudaoming <596167028@qq.com>
2021-01-19 09:03:47 +0800

    添加配置文件

--- micro-services/wings-gateway/src/main/java/com/works/wings/wingsgateway/WingsGatewayApplication.java
+++ micro-services/wings-gateway/src/main/java/com/works/wings/wingsgateway/WingsGatewayApplication.java
@@ -2,8 +2,15 @@ package com.works.wings.wingsgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class WingsGatewayApplication {
 
     public static void main(String[] args) {
