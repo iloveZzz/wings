d098cee51bb2cfb7f53fe8b5ff2f55c74f0aca88
zhudaoming <596167028@qq.com>
2021-01-20 00:04:05 +0800

    配置文件修改

--- micro-services/wings-service-discover/eureka-server/src/main/java/com/works/wings/servicediscover/ServiceDiscoverApplication.java
+++ micro-services/wings-service-discover/eureka-server/src/main/java/com/works/wings/servicediscover/ServiceDiscoverApplication.java
@@ -2,6 +2,9 @@ package com.works.wings.servicediscover;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
