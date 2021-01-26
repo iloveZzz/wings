fe20a4de5625d6aeb9fecc1ffae9ca14937abaa0
zhudaoming <596167028@qq.com>
2021-01-27 02:08:55 +0800

    添加配置文件 支持oauth2

--- micro-services/wings-authentication/src/main/java/com/works/wings/wingssecurity/config/FilterRegisterConfig.java
+++ micro-services/wings-authentication/src/main/java/com/works/wings/wingssecurity/config/FilterRegisterConfig.java
@@ -2,30 +2,15 @@ package com.works.wings.wingssecurity.config;
 
 
 import com.works.wings.wingssecurity.filters.TokenAuthFilter;
-import com.works.wings.wingssecurity.util.DelegatingPasswordEncoderCus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.*;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
-import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
-import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
