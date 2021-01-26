06ed6774505b4d791daad9249297584ec8245cb4
daomingzhu <596167028@qq.com>
2021-01-26 19:14:07 +0800

    录入sentinel规则 调试

--- micro-services/wings-gateway/src/main/java/com/works/wings/wingsgateway/config/SentinelRuleLocators.java
+++ micro-services/wings-gateway/src/main/java/com/works/wings/wingsgateway/config/SentinelRuleLocators.java
@@ -1,36 +1,27 @@
 package com.works.wings.wingsgateway.config;
 
-import com.alibaba.csp.sentinel.datasource.spring.cloud.config.SentinelRuleLocator;
-import com.alibaba.csp.sentinel.datasource.spring.cloud.config.SentinelRuleStorage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.config.client.ConfigClientProperties;
-import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.google.common.collect.Lists;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.PropertySource;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Configuration
 public class SentinelRuleLocators {
 
-    @Autowired
-    private SentinelRuleLocators sentinelRulesSourceLocator;
+    @Bean("dsadasd")
+    @ConfigurationProperties(prefix = "sentinel.flow.rules",ignoreInvalidFields=true)
+    public List<FlowRule> getRules() {
+        return Lists.newArrayList();
+    }
 
-    @Autowired
-    private Environment environment;
-
-    @Autowired
-    private ConfigServicePropertySourceLocator configServicePropertySourceLocator;
-    @Autowired
-    private ConfigClientProperties configClientProperties;
 
     @PostConstruct
-    public void init() {
-        PropertySource<?> locate = configServicePropertySourceLocator.locate(environment);
-        SentinelRuleLocator sentinelRulesSourceLocator = new SentinelRuleLocator(configClientProperties, environment);
-        sentinelRulesSourceLocator.locate(environment);
-        SentinelRuleStorage.retrieveRule("flow_rule");
-
+    public void initRule() {
+        FlowRuleManager.loadRules(getRules());
     }
 }
--- micro-services/wings-gateway/src/main/java/com/works/wings/wingsgateway/test/SpringCouldDataSourceTest.java
+++ micro-services/wings-gateway/src/main/java/com/works/wings/wingsgateway/test/SpringCouldDataSourceTest.java
@@ -7,34 +7,32 @@ import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/test/dataSource/")
 public class SpringCouldDataSourceTest {
 
+    private final List<FlowRule> rules;
 
-    @Autowired
-    private SentinelRuleLocator locator;
+    public SpringCouldDataSourceTest(@Qualifier("dsadasd") List<FlowRule> rules, SentinelRuleLocator locator) {
+        this.rules = rules;
+        this.locator = locator;
+    }
 
-    final Converter<String, List<FlowRule>> converter = new Converter<String, List<FlowRule>>() {
-        @Override
-        public List<FlowRule> convert(String source) {
-            return JSON.parseArray(source, FlowRule.class);
-        }
-    };
+    private final SentinelRuleLocator locator;
 
 
     @GetMapping("/get")
     @ResponseBody
     public List<FlowRule> get() {
-        SpringCloudConfigDataSource dataSource = new SpringCloudConfigDataSource("flow_rule", converter);
-        FlowRuleManager.register2Property(dataSource.getProperty());
         return FlowRuleManager.getRules();
     }
 
